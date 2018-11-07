7464b988f50d94d488c3ba0548f793e609b264ea
ruthmesfun <ruth.mesfun@gmail.com>
2018-11-07 17:11:30 -0500

    All Done

---
+++ lib/insert.sql
@@ -0,0 +1,41 @@
+INSERT INTO series(title, author_id, subgenre_id) VALUES("hi", 1,1);
+INSERT INTO series(title, author_id, subgenre_id) VALUES("hi", 1,1);
+
+INSERT INTO subgenres(name) VALUES ("blah");
+INSERT INTO subgenres(name) VALUES ("blah");
+
+INSERT INTO authors(name) VALUES ("hm");
+INSERT INTO authors(name) VALUES ("hm");
+
+INSERT INTO books (title, year, series_id) VALUES("hmm", 2004, 1);
+INSERT INTO books (title, year, series_id) VALUES("hmm", 2004, 1);
+INSERT INTO books (title, year, series_id) VALUES("hmm", 2004, 1);
+INSERT INTO books (title, year, series_id) VALUES("hmm", 2004, 1);
+INSERT INTO books (title, year, series_id) VALUES("hmm", 2004, 1);
+INSERT INTO books (title, year, series_id) VALUES("hmm", 2004, 1);
+
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES('git.', "na", "why me", 1,1);
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES('git.', "na", "why me", 1,1);
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES('git.', "na", "why me", 1,1);
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES('git.', "na", "why me", 1,1);
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES('git.', "na", "why me", 1,1);
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES('git.', "na", "why me", 1,1);
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES('git.', "na", "why me", 1,1);
+INSERT INTO characters (name, species, motto, series_id, author_id) VALUES('git.', "na", "why me", 1,1);
+
+INSERT INTO character_books(book_id, character_id) VALUES(1,1);
+INSERT INTO character_books(book_id, character_id) VALUES(1,1);
+INSERT INTO character_books(book_id, character_id) VALUES(1,1);
+INSERT INTO character_books(book_id, character_id) VALUES(1,1);
+INSERT INTO character_books(book_id, character_id) VALUES(1,1);
+INSERT INTO character_books(book_id, character_id) VALUES(1,1);
+INSERT INTO character_books(book_id, character_id) VALUES(1,1);
+INSERT INTO character_books(book_id, character_id) VALUES(1,1);
+INSERT INTO character_books(book_id, character_id) VALUES(1,1);
+INSERT INTO character_books(book_id, character_id) VALUES(1,1);
+INSERT INTO character_books(book_id, character_id) VALUES(1,1);
+INSERT INTO character_books(book_id, character_id) VALUES(1,1);
+INSERT INTO character_books(book_id, character_id) VALUES(1,1);
+INSERT INTO character_books(book_id, character_id) VALUES(1,1);
+INSERT INTO character_books(book_id, character_id) VALUES(1,1);
+INSERT INTO character_books(book_id, character_id) VALUES(1,1);
